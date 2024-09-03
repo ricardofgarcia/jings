24a2fa3f579d8782897505c6f7e00b5ee43a8bb2
Ric Garcia <ricardofgarcia@gmail.com>
2024-09-03 14:09:42 -0400

    Update JiraApiCalls.js
    Documentation updates

--- JiraApiCalls.js
+++ JiraApiCalls.js
@@ -1,7 +1,9 @@
   /**
-   * TODO: Add Documentation
+   * Fetches the profile info of the personal access token owner.
+   * 
+   * @returns {Object}        - An object with the user profile or null if there was a failure obtaining it
    */
-function fetchCurrentUserProfileInfo(){
+  function fetchCurrentUserProfileInfo(){
     try{
         var jiraServer  = getJiraServer();
         var jiraPAT   = getJiraPAT();
@@ -16,7 +18,7 @@ function fetchCurrentUserProfileInfo(){
         var userProfileInfo;
       
         var response = UrlFetchApp.fetch(encodeURI(apiUrl), options);
-  /**/    
+
         if (response.getResponseCode() < 400) {
           //console.log("SUCCESS! " + response.getResponseCode());
           var contextText = response.getContentText();
@@ -25,7 +27,7 @@ function fetchCurrentUserProfileInfo(){
           //console.log("displayName: %s",displayName);
           //console.log(responseData);
         }
-  /**/
+
         return userProfileInfo;
     }catch (err) {
         // TODO (developer) - Handle exception
@@ -35,7 +37,10 @@ function fetchCurrentUserProfileInfo(){
   }
 
 /**
- * TODO: Add Documentation
+ * Gets headers needed for API call
+ * 
+ * @param   {String} jiraPAT  - Personal Access token of a Jira user
+ * @returns {Object}          - A key-value pair of header properties
  */
 function getJiraApiCallHeaders(jiraPAT){
     var headers = { 'Authorization' : 'Bearer ' + jiraPAT};
@@ -43,26 +48,92 @@ function getJiraApiCallHeaders(jiraPAT){
 }
 
 /**
- * TODO: Add Documentation
+ * Gets options needed to make API call
+ * @returns                 - A key-value pair of header properties
  */
-function getJiraApiCallOptions(method,headers){
- 
-    var options = {
-        "async": true,
-        'method' : method,
-        'accept': 'application/json',
-        'contentType': 'application/json',
-        'headers' : headers,
-        'muteHttpExceptions' : false
-      };
+function getJiraApiCallOptions(method,headers,data=null){
+
+  var options;
+
+  options = {
+      "async": true,
+      'method' : method,
+      'accept': 'application/json',
+      'contentType': 'application/json',
+      'headers' : headers,
+      'muteHttpExceptions' : true
+    };
+
+    if (data != null){
+      options['payload'] = JSON.stringify(data);
+    }
+
     return options;
 }
 
 /**
 * TODO: Add Documentation
-* Fetches a maximum of 1000 results.
+* By default, it fetches a maximum of 1000 results.
 */
+/**
+ * Fetches info about issues returned by the given jql.
+ * 
+ * @param     jql         - A Jira Query Language statement
+ * 
+ * @returns   {Object}    - An object containing the information returned by the API
+ * 
+ * @throws                - Passes through exceptions thrown by called functions
+ */
 function fetchJiraIssuesInfo(jql,startAt=0,maxResults=1000){
+
+      var jiraData;
+
+      var jiraServer  = getJiraServer();
+      var jiraPAT   = getJiraPAT();
+
+      var headers = getJiraApiCallHeaders(jiraPAT);
+      var fields      = getJiraFieldsToFetch_FieldId_asArray();
+      var method  = 'POST';
+
+      var data = {};
+      data["jql"]         = jql;
+      data["fields"]      = fields;
+      data["startAt"]     = startAt;
+      data["maxResults"]  = maxResults;        
+
+      var options = getJiraApiCallOptions(method,headers,data);
+
+      const apiPath     = "/rest/api/2/search";
+      //const apiParams  = 'jql='+jql;
+
+      const apiUrl      = 'https://' + jiraServer + apiPath;
+      //var requestUrl = api_url;
+
+      //var requestUrl = apiUrl + apiParams + "&fields=" + fields + '&maxResults=' + maxResults + '&startAt=' + startAt;
+  
+      var response = UrlFetchApp.fetch(encodeURI(apiUrl), options);
+      if (response.getResponseCode() < 400) {
+          var contextText = response.getContentText();
+          jiraData = JSON.parse(contextText);
+      }
+      return jiraData;
+}
+
+/**
+* TODO: Add Documentation
+* By default, it fetches a maximum of 1000 results.
+*/
+/**
+ * Fetches info about issues returned by the given jql. Good for short JQL statements since it uses the "GET" method.
+ * 
+ * @param     jql         - A Jira Query Language statement
+ * 
+ * @returns   {Object}    - An object containing the information returned by the API
+ * 
+ * @throws                - Passes through exceptions thrown by called functions
+ */
+
+function fetchJiraIssuesInfo_UsingGet(jql,startAt=0,maxResults=1000){
     //try{
         var jiraData;
 
